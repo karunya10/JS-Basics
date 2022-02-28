df78cc8b4b3d73fb477a58db1a805a109611d0b9
Sakthi Karunya <sak@Sakthis-MacBook-Air.local>
2022-02-28 08:10:00 +0530

    Maximum of Two Numbers

---
+++ maxNumber.js
@@ -0,0 +1,13 @@
+function maxNumber(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+
+let n1 = 5;
+let n2 = 6;
+let number = maxNumber(n1, n2);
+
+console.log(number);
